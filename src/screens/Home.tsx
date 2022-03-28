c39e40177a2ac6e8f0418dfc53e4fe6d7aed63d0
victor19pa <vpinedaa@unah.hn>
2022-03-27 23:26:16 -0600

    :sparkles: feat: tipado movieDB

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,18 +1,19 @@
 import { useNavigation } from '@react-navigation/native'
 import React, { useEffect } from 'react'
-import { Button, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import movieDB from '../api/movieDB'
+import { MovieDBNowPlaying } from '../interfaces/movieInterface'
 
 const Home = () => {
 
   useEffect(() => {
-    movieDB.get('/now_playing')
+    movieDB.get<MovieDBNowPlaying>('/now_playing')
       .then(res => {
-        console.log(res.data);
+        console.log(res.data.results[0]);
+        
       })
   }, [])
   
-
   return (
     <View>
       <Text style={{color: 'black'}}>
