3b02d056f1d7337e7f3cb228d2964da4b8b58f3c
victor19pa <vpinedaa@unah.hn>
2022-03-28 22:44:19 -0600

    :sparkles: feat: carousel

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import { ActivityIndicator, Text, View } from 'react-native'
-import { useMovies } from '../hooks/useMovies'
-import useState from 'react';
+import React, { useState } from 'react'
+import { ActivityIndicator, Dimensions, StyleSheet, Text, View } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+import { useMovies } from '../hooks/useMovies';
 import MoviePoster from '../components/MoviePoster';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+const { width: windowWidth } = Dimensions.get('window');
 
 const Home = () => {
   const { top } = useSafeAreaInsets();
@@ -18,11 +20,24 @@ const Home = () => {
   }
   return (
     <View style={{ marginTop: top + 15 }}>
-      <MoviePoster 
+      {/* <MoviePoster 
         movie={ peliculasEnCine[3]} 
-      />
+      /> */}
+      <View style={styles.carouselContainer}>
+        <Carousel
+          data={peliculasEnCine}
+          renderItem={({ item }: any) => <MoviePoster movie={item} />}
+          sliderWidth={windowWidth}
+          itemWidth={300}
+        />
+      </View>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  carouselContainer:{
+    height: 440
+  }
+});
 
 export default Home
