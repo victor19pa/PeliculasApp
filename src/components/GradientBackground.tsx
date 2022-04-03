2293373ccd5f300a0c8a2b7c99f057d0fe889595
victor19pa <vpinedaa@unah.hn>
2022-04-03 15:40:56 -0600

    :sparkles: feat: usar colores del context

--- src/components/GradientBackground.tsx
+++ src/components/GradientBackground.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet, View } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
+import { GradientContext } from '../context/GradientContext'
 
 interface Props {
   children: JSX.Element | JSX.Element[]
 }
 
 const GradientBackground = ({ children }: Props) => {
+
+  const { colors } = useContext(GradientContext);
+  
   return (
     <View style={{ flex: 1, backgroundColor: '#084F6A' }}>
       <LinearGradient
-        colors={['#084f6a', '#75cedb', '#fff']}
+        colors={[ colors.primary, colors.secondary, '#fff']}
         style={{ ...StyleSheet.absoluteFillObject }}
         start={{ x: 0.1, y: 0.1 }}
         end={{ x: 0.5, y: 0.7 }}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { ActivityIndicator, Dimensions, ScrollView, StyleSheet, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
@@ -6,24 +6,24 @@ import { useMovies } from '../hooks/useMovies';
 import ImageColors from 'react-native-image-colors'
 import MoviePoster from '../components/MoviePoster';
 import HorizontalSlider from '../components/HorizontalSlider';
-import GradientBackground from './GradientBackground';
+import GradientBackground from '../components/GradientBackground';
 import { getImageColors } from '../helpers/getColors';
+import { GradientContext, GradientProvider } from '../context/GradientContext';
 
 const { width: windowWidth } = Dimensions.get('window');
 
 const Home = () => {
   const { top } = useSafeAreaInsets();
   const { nowPlaying, popular, topRated, upcoming, isLoading } = useMovies();
+  const { setMainColors, setPrevMainColors } = useContext(GradientContext);
 
   const getPosterColors = async (index: Number) => {
     const movie = nowPlaying[index]
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
-    const [primary, secondary] = await getImageColors(uri);
+    const [primary = '#fff', secondary = '#fff'] = await getImageColors(uri);
 
-    console.log(primary);
-    console.log(secondary);
-    
+    setMainColors({ primary, secondary })
   }
 
   if (isLoading) {
